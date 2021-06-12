1132ced15a7d449d601973a551106ad4d4aec554
roznerx <ignaciorosner@gmail.com>
2021-06-11 23:28:06 -0300

    Continued working the clear and resize buttons, with that last one producing many errors to be fixed

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -9,13 +9,55 @@ function createGrid(n) {
     }
 }
 
+function clearGrid() {
+
+    const squares = Array.from(container.childNodes);
+    squares.forEach((square) => {
+    container.removeChild(square);
+    });
+
+}
+
 createGrid(256);
 
+const clear = document.getElementById("clear");
+const resize = document.getElementById("resize");
 const square = document.querySelectorAll(".square");
+
 square.forEach(function(elem) {
+
     elem.addEventListener("mouseover", function(event) {
         event.target.style.backgroundColor = "black";
         console.log("Event triggered");
     });
 });
 
+clear.addEventListener("click", function() {
+
+    for (i = 0; i < square.length; i++) {
+        if (square[i].style.backgroundColor = "black") {
+            square[i].style.backgroundColor = "white";
+            console.log("Board cleared");
+        }
+    }
+});
+
+resize.addEventListener("click", function() {
+
+    clearGrid()
+
+    const squaresPerSide = prompt("Select number of squares per side");
+    let inputNum = parseFloat(squaresPerSide);
+
+    if (inputNum > 100) {
+
+        return prompt("Select number of squares per side");
+        
+    } else if (inputNum != null) {
+        
+        createGrid(inputNum * inputNum);
+        container.style.setProperty('grid-template-columns', 'repeat(' + inputNum + ', 25px)');
+        container.style.setProperty('grid-template-rows', 'repeat(' + inputNum + ', 25px)');
+        console.log("Board resized to " + inputNum + " squares per side");
+    }
+});
